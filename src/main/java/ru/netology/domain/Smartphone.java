5831837585980ffffecee8623a4718869c17d8fa
taya sapurina <sapurina0t@gmail.com>
2022-08-02 22:24:20 +0500

    new method is added

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -15,4 +15,14 @@ public class Smartphone extends Product{
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
+
+    public boolean matches(String search) {
+        if (super.matches(search)) { // вызов метода matches в версии описанной в Product
+            return true;
+        }
+        if (getManufacturer().matches(search)) {
+            return true;
+        }
+        return false;
+    }
 }
